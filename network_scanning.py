d203500d61804590448a378365145394ec126d7e
beik <sam.beik@ties.k12.mn.us>
2019-05-01 09:06:48 -0500

    Changed rate, added date to file! Changed nmap output to xml in order to import into elastic search.

--- network_scanning.py
+++ network_scanning.py
@@ -3,17 +3,17 @@
 
 import re
 from subprocess import call, Popen, PIPE
+import datetime
 
 
-masscan = 'cat swt.txt | xargs masscan --rate 100000 -oL /var/log/masscan-output.log \
+masscan = 'cat swt.txt | xargs masscan --rate 10000 -oL /var/log/masscan-output.log \
                 --append-output --ping'
 
 p = Popen(masscan, stdout=PIPE, shell=True)
 (output, err) = p.communicate()
 p_status = p.wait()
-print("Masscan Output: " + str(output))
 
-
+current_date = datetime.datetime.today().strftime("%m-%d-%Y")
 masscan_results = open('/var/log/masscan-output.log', "r")
 ping_systems = open('/var/log/live_systems.log', 'w')
 ip_regex = re.compile(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})")
@@ -25,10 +25,11 @@ for line in enumerate(masscan_results):
         # print(ip)
         ip_array.append(ip) # YAY!
         ping_systems.write("%s\n" % ip)
+        ascending_file = sorted(ping_systems)
 ping_systems.close()
 masscan_results.close()
 
-nmap = call("nmap -iL /var/log/live_systems.log -A -oN /var/log/nmap_results.log", shell=True)
+nmap = call("nmap -iL /var/log/live_systems.log -A -oX /var/log/" + current_date + " nmap_results.log", shell=True)
 
 
 
